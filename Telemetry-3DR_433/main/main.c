44c6112c4f15903c5f22011721df0be561b376d9
iron_kang <iron.kcj@gmail.com>
2018-07-03 21:30:21 +0800

    PC <-> ESP32S communicate

--- Telemetry-3DR_433/main/main.c
+++ Telemetry-3DR_433/main/main.c
@@ -7,8 +7,8 @@
 #include "driver/gpio.h"
 #include "driver/uart.h"
 
-#define Telemetry_TX	  12
-#define Telemetry_RX      27
+#define Telemetry_TX	  17
+#define Telemetry_RX      16
 
 #define header_H    0x55 //Header
 #define header_L    0xAA //Header
@@ -16,18 +16,44 @@
 #define data_Length 0x00 //Data length
 #define get_Dis_CMD 0x02 //Command: Read Distance
 #define checksum    (header_H+header_L+device_Addr+data_Length+get_Dis_CMD) //Checksum
-unsigned char i=0;
-unsigned int  Distance=0;
-unsigned char Rx_DATA[8];
-char CMD[6]={
-  header_H,header_L,device_Addr,data_Length,get_Dis_CMD,checksum}; //Distance command package
+
+typedef struct {
+	 float x;
+	 float y;
+	 float z;
+} AXIS;
+
+typedef struct _gps_data {
+	 float utc_time;
+	 float latitude;
+	 char  latitude_ch;
+	 float longitude;
+	 char  longitude_ch;
+	 int   status;
+	 int   num;
+	 float hdop;
+	 float altitude;
+	 float height;
+} GPS_Data;
+
+typedef struct _info {
+ 	AXIS attitude;
+ 	float thrust[4];
+ 	float bat;
+ 	unsigned int height;
+// 	uint8_t status;
+// 	GPS_Data gps;
+ }Info;
+
 
 void app_main()
 {
     int len = 0;
+    Info info;
+    char buf[100];
 
     uart_config_t uart_config = {
-        .baud_rate = 57600,
+        .baud_rate = 115200,
         .data_bits = UART_DATA_8_BITS,
         .parity = UART_PARITY_DISABLE,
         .stop_bits = UART_STOP_BITS_1,
@@ -43,11 +69,33 @@ void app_main()
                  UART_PIN_NO_CHANGE);
     uart_driver_install(UART_NUM_1, 1024 * 2, 0, 0, NULL, 0);
     vTaskDelay(100 / portTICK_RATE_MS);
+
+    info.attitude.x = 1;
+    info.attitude.y = 0.3;
+    info.attitude.z = 0.4;
+
+    uint8_t size = sizeof(Info);
+    char cmd[2] = {'#', '$'};
     printf("init finish\n");
 
     while(1) {
-        uart_write_bytes(UART_NUM_1, CMD, 6);
-        vTaskDelay(150 / portTICK_RATE_MS);
+        buf[0] = 0;
+        uart_read_bytes(UART_NUM_1, (uint8_t *)&buf[0], 1, 1000 / portTICK_RATE_MS);
+        if (buf[0] == '@')
+        {
+            info.attitude.x++;
+            buf[size] = 0;
+            uart_write_bytes(UART_NUM_1, cmd, 2);
+            memcpy(buf, &info, sizeof(Info));
+            for (int i = 0; i < size; i++)
+                buf[size] += buf[i];
+
+            uart_write_bytes(UART_NUM_1, buf, size+1);
+
+        }
+        else vTaskDelay(50 / portTICK_RATE_MS);
+        uart_flush(UART_NUM_1);
+        
 
 
     }
